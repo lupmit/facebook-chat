2318357fae268093e140b7b7239cfaaf174a1393
LUPmit <44107677+LUPmit@users.noreply.github.com>
2018-11-26 16:44:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,15 @@
 
 var request = require("request");
 var login = require("facebook-chat-api");
+var mess = new Array("bạn nói cái gì v ạ?","ý bạn là gì","nhắn cái gì có ý nghĩa cái coi ><","đừng spam nuwaxx má ơi-.-")
+const fs = require("fs");
 var botkey = "http://ghuntur.com/simsim.php?lc=vn&deviceId=&bad=0&txt=";
+
 login(
-	{	
-	email: "lexuanphuc.only1", 
-	password: "phuc22111999" 
+	{
+	appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))
 	},
+	
 function callback (err, api)
 {
 	if(err) return console.error(err);
@@ -19,7 +22,7 @@ function callback (err, api)
 	
 	api.listen(function callback(err, message)
 	{
-		if(message.body === "/llooggoouutt") { 
+		if(message.body === "/logout") { 
 			api.sendMessage(";) Ngừng auto chat thành công.", message.threadID); 
 			api.markAsRead(message.threadID);
 			return api.logout(err);
@@ -31,6 +34,10 @@ function callback (err, api)
 			api.markAsRead(message.threadID); 
 			console.log("Sender ID: " + message.senderID);
 		}
+		else if (message.body.indexOf("stop")===0){
+			console.log("FormID: " + message.threadID + '->Message: '+message.body);
+			return;
+		}
 		else if (message.body)
 		{
 			request(botkey + encodeURI(message.body),  
@@ -38,6 +45,14 @@ function callback (err, api)
 			{  
 				let text = '';
 				text += body;
+				text = text.replace(/Gà Símimi/gi,"phúc");
+				text = text.replace(/sim sim/gi,"phúc");
+				text = text.replace(/simsimi/gi,"phúc");
+				text = text.replace(/simsi/gi,"phúc");
+				text = text.replace(/Pooh/gi,"phúc");
+				text = text.replace(/símimi/gi,"phúc");
+				text = text.replace(/simi/gi,"phúc");
+				text = text.replace("Talk with random person: https://play.google.com/store/apps/details?id=www.speak.com", mess[Math.floor(Math.random() * 3)]);
 				console.log(text.respSentence);
 				api.sendMessage(text, message.threadID);					
 			});

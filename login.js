9729296897e78a230c18c0763d7e3af486fb9b9f
LUPmit <44107677+LUPmit@users.noreply.github.com>
2018-11-26 16:06:12 +0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,10 @@
+﻿const fs = require("fs");
+const login = require("facebook-chat-api");
+
+var credentials = {email: "lupmit", password: "lupmit219"};
+
+login(credentials, (err, api) => {
+    if(err) return console.error(err);
+
+    fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
+});
